1bcebd4fb66915e438c1ddf4f709733b1b7d910a
patrbng72 <patrick@patrickbingham.net>
2020-08-27 15:22:50 -0500

    updated createtable

--- createtable.ps1
+++ createtable.ps1
@@ -7,7 +7,14 @@ Select-AzSubscription -Subscription $subscriptionName
 $resourcegroup = "stxcustmetadata"
 $storageaccount = "stxcustmetadata"
 
-$ctx = Get-AzStorageAccount -ResourceGroupName $resourcegroup -Name $storageaccount
+$storageaccountkey = (Get-AzStorageAccountKey -ResourceGroupName $resourcegroup -Name $storageaccount)[0].Value
+$ctx = New-AzStorageContext -StorageAccountName $storageaccount -StorageAccountKey $storageaccountkey
 
-$tableName = "main_table"
+$tablename = "maintable"
 New-AzStorageTable -Name $tableName -Context $ctx
+
+# $SAS = New-AzStorageAccountSASToken -Service Table -ResourceType Container,Service,Object -StartTime (Get-Date) -ExpiryTime (Get-Date).AddHours(6) -Context $ctx -Permission 'rwdlau'
+
+$cloudtable = (Get-AzStorageTable -Name $tablename -Context $ctx).CloudTable
+Add-AzTableRow -Table $cloudtable -PartitionKey 'main' -RowKey ((New-Guid).Guid) -property @{ Result = 'Success'; Source = 'PipeHow' }
+Add-AzTableRow -Table $cloudtable -PartitionKey 'subscription' -RowKey ((New-Guid).Guid) -property @{ Result = 'Failure'; Source = 'PipeHow' }
